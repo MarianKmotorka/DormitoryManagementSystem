dc7800435aa22b49374969744375ebd6efae7e06
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-02-21 15:18:04 +0100

    Basic Layout

---
+++ DormitoryManagementSystem/WpfClient/Bootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Caliburn.Micro;
+using WpfClient.ViewModels;
+
+namespace WpfClient
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private readonly SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+            LoadLanguage();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IWindowManager, WindowManager>();
+
+            GetType().Assembly.GetTypes()
+                .Where(t => t.IsClass)
+                .Where(t => t.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(t => _container.RegisterPerRequest(t, t.ToString(), t));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        private void LoadLanguage()
+        {
+            var dictionary = new ResourceDictionary
+            {
+                Source = new Uri("..\\Resources\\lang.en.xaml", UriKind.Relative)
+            };
+
+            _container.RegisterInstance(typeof(ResourceDictionary), "language", dictionary);
+
+            Application.Current.Resources.MergedDictionaries.Add(dictionary);
+        }
+    }
+}
---
+++ DormitoryManagementSystem/WpfClient/ViewModels/ShellViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace WpfClient.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly SimpleContainer _simpleContainer;
+        private readonly ResourceDictionary _language;
+        private bool _isEnglish = true;
+        private bool _isSlovak = false;
+
+        public bool IsEnglish
+        {
+            get => _isEnglish;
+            set { _isEnglish = value; NotifyOfPropertyChange(nameof(IsEnglish)); }
+        }
+
+        public bool IsSlovak
+        {
+            get => _isSlovak;
+            set { _isSlovak = value; NotifyOfPropertyChange(nameof(IsSlovak)); }
+        }
+
+
+        public ShellViewModel(IEventAggregator eventAggregator, SimpleContainer simpleContainer)
+        {
+            _eventAggregator = eventAggregator;
+            _simpleContainer = simpleContainer;
+            _language = IoC.Get<ResourceDictionary>("language");
+        }
+
+        public void ChangeLanguage(string language)
+        {
+            _simpleContainer.UnregisterHandler<ResourceDictionary>("language");
+            Application.Current.Resources.MergedDictionaries.Remove(_language);
+
+            var dictionary = new ResourceDictionary
+            {
+                Source = new Uri($"..\\Resources\\lang.{language}.xaml", UriKind.Relative)
+            };
+
+            _simpleContainer.RegisterInstance(typeof(ResourceDictionary), "language", dictionary);
+            Application.Current.Resources.MergedDictionaries.Add(dictionary);
+
+            if (language == "sk")
+                IsEnglish = false;
+            else
+                IsSlovak = false;
+        }
+
+        public void OpenTab(string name)
+        {
+
+        }
+    }
+}
