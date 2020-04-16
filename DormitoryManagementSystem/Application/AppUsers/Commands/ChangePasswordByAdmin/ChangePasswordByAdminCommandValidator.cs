bde2a0f91d4ca185343ddc6bdf929a623f86b446
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-04-16 19:53:08 +0200

    Change password validator fix

--- DormitoryManagementSystem/Application/AppUsers/Commands/ChangePasswordByAdmin/ChangePasswordByAdminCommandValidator.cs
+++ DormitoryManagementSystem/Application/AppUsers/Commands/ChangePasswordByAdmin/ChangePasswordByAdminCommandValidator.cs
@@ -1,16 +1,18 @@
-﻿using Application.Common.Models;
+﻿using System.Linq;
+using Application.Common.Models;
 using FluentValidation;
 
 namespace Application.AppUsers.Commands.ChangePasswordByAdmin
 {
-    public class ChangePasswordByAdminCommandValidator : AbstractValidator<ChangePasswordByAdminCommand>
-    {
-        public ChangePasswordByAdminCommandValidator()
-        {
-            RuleFor(x => x.NewPassword).Cascade(CascadeMode.StopOnFirstFailure)
-                .NotEmpty().WithMessage(ErrorMessages.Required)
-                .MinimumLength(6).WithMessage(ErrorMessages.MinLength).WithState(_ => 6)
-                .MaximumLength(30).WithMessage(ErrorMessages.MinLength).WithState(_ => 30);
-        }
-    }
+	public class ChangePasswordByAdminCommandValidator : AbstractValidator<ChangePasswordByAdminCommand>
+	{
+		public ChangePasswordByAdminCommandValidator()
+		{
+			RuleFor(x => x.NewPassword).Cascade(CascadeMode.StopOnFirstFailure)
+				.NotEmpty().WithMessage(ErrorMessages.Required)
+				.MinimumLength(6).WithMessage(ErrorMessages.MinLength).WithState(_ => 6)
+				.MaximumLength(30).WithMessage(ErrorMessages.MinLength).WithState(_ => 30)
+				.Must(x => x.ToList().Any(char.IsLetter)).WithMessage(ErrorMessages.MustContainLetter);
+		}
+	}
 }
--- DormitoryManagementSystem/Application/Common/Models/ErrorMessages.cs
+++ DormitoryManagementSystem/Application/Common/Models/ErrorMessages.cs
@@ -1,33 +1,34 @@
 ﻿namespace Application.Common.Models
 {
-    public static class ErrorMessages
-    {
-        public const string Invalid = nameof(Invalid);
-        public const string InvalidEmail = nameof(InvalidEmail);
-        public const string Required = nameof(Required);
-        public const string MinLength = nameof(MinLength);
-        public const string MaxLength = nameof(MaxLength);
+	public static class ErrorMessages
+	{
+		public const string Invalid = nameof(Invalid);
+		public const string InvalidEmail = nameof(InvalidEmail);
+		public const string Required = nameof(Required);
+		public const string MinLength = nameof(MinLength);
+		public const string MaxLength = nameof(MaxLength);
+		public const string MustContainLetter = nameof(MustContainLetter);
 
-        public const string JwtIsNotExpired = nameof(JwtIsNotExpired);
-        public const string RefreshTokenDoesNotExist = nameof(RefreshTokenDoesNotExist);
-        public const string RefreshTokenExpired = nameof(RefreshTokenExpired);
-        public const string RefreshTokenAlreadyUsed = nameof(RefreshTokenAlreadyUsed);
-        public const string RefreshTokenDoesNotMatchJwt = nameof(RefreshTokenDoesNotMatchJwt);
+		public const string JwtIsNotExpired = nameof(JwtIsNotExpired);
+		public const string RefreshTokenDoesNotExist = nameof(RefreshTokenDoesNotExist);
+		public const string RefreshTokenExpired = nameof(RefreshTokenExpired);
+		public const string RefreshTokenAlreadyUsed = nameof(RefreshTokenAlreadyUsed);
+		public const string RefreshTokenDoesNotMatchJwt = nameof(RefreshTokenDoesNotMatchJwt);
 
-        public const string AlreadyConfirmed = nameof(AlreadyConfirmed);
-        public const string EmailNotFound = nameof(EmailNotFound);
-        public const string EmailNotUnique = nameof(EmailNotUnique);
-        public const string InvalidEmailOrPassword = nameof(InvalidEmailOrPassword);
-        public const string EmailNotConfirmed = nameof(EmailNotConfirmed);
+		public const string AlreadyConfirmed = nameof(AlreadyConfirmed);
+		public const string EmailNotFound = nameof(EmailNotFound);
+		public const string EmailNotUnique = nameof(EmailNotUnique);
+		public const string InvalidEmailOrPassword = nameof(InvalidEmailOrPassword);
+		public const string EmailNotConfirmed = nameof(EmailNotConfirmed);
 
-        public const string StartDateMustOccurBeforeEndDate = nameof(StartDateMustOccurBeforeEndDate);
-        public const string MustBeInTheFuture = nameof(MustBeInTheFuture);
-        public const string DateRangeOverlapsWithExisingRequest = nameof(DateRangeOverlapsWithExisingRequest);
+		public const string StartDateMustOccurBeforeEndDate = nameof(StartDateMustOccurBeforeEndDate);
+		public const string MustBeInTheFuture = nameof(MustBeInTheFuture);
+		public const string DateRangeOverlapsWithExisingRequest = nameof(DateRangeOverlapsWithExisingRequest);
 
-        public const string RoomMustBeAvailable = nameof(RoomMustBeAvailable);
-        public const string OfficeMustBeAvailable = nameof(OfficeMustBeAvailable);
+		public const string RoomMustBeAvailable = nameof(RoomMustBeAvailable);
+		public const string OfficeMustBeAvailable = nameof(OfficeMustBeAvailable);
 
-        public const string RepairAlreadyRequested = nameof(RepairAlreadyRequested);
-        public const string CannotModifyFixed = nameof(CannotModifyFixed);
-    }
+		public const string RepairAlreadyRequested = nameof(RepairAlreadyRequested);
+		public const string CannotModifyFixed = nameof(CannotModifyFixed);
+	}
 }
